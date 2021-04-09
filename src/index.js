5643ae9fbb90a4a167abcc3c13399459b662116e
Thulasiraman-Dev <e.thulasiraman006@gmail.com>
2021-04-09 10:23:14 +0500

    added education

---
+++ src/index.js
@@ -0,0 +1,20 @@
+function tenthstandatd() {
+  document.getElementById("aboutmetext").innerHTML = "2015-2016";
+  document.getElementById("aboutmedescriptiontext").innerHTML =
+    "10th standrad" + "<br>TVS Matriculation Higher Secondary School";
+  document.getElementById("aboutmedescriptiontext").style.fontSize = "30px";
+  document.getElementById("aboutmedescriptiontext").style.animation="textanimation 800ms 1";
+}
+function twelthstandatd() {
+  document.getElementById("aboutmetext").innerHTML = "2017-2018";
+  document.getElementById("aboutmedescriptiontext").innerHTML =
+    "12th standrad" + "<br>TVS Matriculation Higher Secondary School";
+  document.getElementById("aboutmedescriptiontext").style.fontSize = "30px";
+  document.getElementById("aboutmedescriptiontext").style.animation="textanimation 800ms 1";
+}
+function college() {
+  document.getElementById("aboutmetext").innerHTML = "2018-2022";
+  document.getElementById("aboutmedescriptiontext").innerHTML =
+    "B.E.Computer Science Engineering" + "<br>Velammal College of Engineering and Technology";
+  document.getElementById("aboutmedescriptiontext").style.fontSize = "30px";
+}
